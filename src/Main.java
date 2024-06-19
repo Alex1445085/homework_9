a6f2f9de07e031617031ab97931379dd5b3c778f
Alex1445085 <n1445085@yandex.ru>
2024-06-19 16:56:01 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+ // 1.
+        System.out.println("\n1.");
+        int [] consumption = new int[5];
+        for (int i = 0; i < consumption.length; i++) {
+            double randomDouble = Math.random() * 40000 + 10000;
+            consumption[i] = (int) randomDouble;
+        }
+        System.out.println(Arrays.toString(consumption));
+        int totalConsumption = 0;
+        for (int content : consumption) {
+            totalConsumption += content;
+        }
+        System.out.printf("Сумма трат за месяц составила %d рублей.\n", totalConsumption);
     }
-
 }
